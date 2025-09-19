14f0ec77ad03299f442346ff848fafc78dc2e71a
Edwin Mijatovic Garcia Montelongo <56989leo@universidaddeleon.edu.mx>
2025-09-19 07:41:51 -0600

    Version 1 - 19/09/25 - 07:41

---
+++ clases/avanzadas.py
@@ -0,0 +1,89 @@
+class Operaciones:
+    def __init__(self):
+        self.num1 = 0
+        self.num2 = 0
+        self.resultado = 0
+        
+    def leerNumeros(self):
+        while True:
+            try:
+                self.num1 = int(input("Número 1: "))
+                break
+            except Exception:
+                print("Número inválido")
+                continue
+        while True:
+            try:
+                self.num2 = int(input("Número 2: "))
+                break
+            except Exception:
+                print("Número inválido")
+                continue    
+    
+    def sumar(self):
+        self.resultado = "La suma de " + str(self.num1) + " + " + str(self.num2) + " es igual a " + str(self.num1 + self.num2)
+    
+    def restar(self):
+        self.resultado = "La resta de " + str(self.num1) + " - " + str(self.num2) + " es igual a " + str(self.num1 - self.num2)
+    
+    def multiplicar(self):
+        self.resultado = "La multiplicación de " + str(self.num1) + " * " + str(self.num2) + " es igual a " + str(self.num1 * self.num2)
+    
+    def dividir(self):
+        if self.num2 == 0:
+            self.resultado = "Error: No se puede dividir entre cero"
+        else:
+            self.resultado = "La división de " + str(self.num1) + " / " + str(self.num2) + " es igual a " + str(self.num1 / self.num2)
+    
+    def modulo(self):
+        if self.num2 == 0:
+            self.resultado = "Error: No se puede calcular el módulo con divisor cero"
+        else:
+            self.resultado = "El módulo de " + str(self.num1) + " % " + str(self.num2) + " es igual a " + str(self.num1 % self.num2)
+    
+    def mostrarResultado(self):
+        print(self.resultado)
+
+
+# Función principal para ejecutar la calculadora
+def main():
+    calculadora = Operaciones()
+    
+    while True:
+        print("\n=== CALCULADORA ===")
+        print("1. Sumar")
+        print("2. Restar")
+        print("3. Multiplicar")
+        print("4. Dividir")
+        print("5. Módulo")
+        print("6. Salir")
+        
+        opcion = input("Seleccione una opción (1-6): ")
+        
+        if opcion == "6":
+            print("¡Hasta luego!")
+            break
+        
+        if opcion in ["1", "2", "3", "4", "5"]:
+            calculadora.leerNumeros()
+            
+            if opcion == "1":
+                calculadora.sumar()
+            elif opcion == "2":
+                calculadora.restar()
+            elif opcion == "3":
+                calculadora.multiplicar()
+            elif opcion == "4":
+                calculadora.dividir()
+            elif opcion == "5":
+                calculadora.modulo()
+            
+            calculadora.mostrarResultado()
+        else:
+            print("Opción inválida. Por favor, seleccione 1-6.")
+
+# Ejecutar el programa
+if __name__ == "__main__":
+    main()
+        
+    
